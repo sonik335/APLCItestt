376201b4988be957f43163ac9db2c27362193388
Kirill <sonik3000.83@gmail.com>
2022-11-21 14:02:19 +0300

    initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,12 +2,13 @@ package ru.netology.rest;
 
 
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 
 public class MobileBankApiTestV1 {
@@ -19,12 +20,30 @@ public class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 .then()
                 .statusCode(200);
-                //.body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+
 
 
 
 
     }
+
+    @Test
+    void shouldReturnDemoAccounts1() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
+
+
+
+
+
 
 
 
@@ -35,9 +54,37 @@ public class MobileBankApiTestV1 {
                 .when()
                 .get("/demo/accounts")
                 .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("",hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
-                .body("[1].currency", equalTo("USD"));
+                .body("[0].balance", greaterThanOrEqualTo(0));
     }
+
+    @Test
+    void shouldReturnDemoAccounts2() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }",is(true));
+
+    }
+
+    @Test
+    void shouldReturnDemoAccounts3 () {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
 
 
 }
